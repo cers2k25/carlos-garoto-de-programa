a58a070affbfb31e7a7539e36cdd95c07d9d749d
cers2k25 <i4364165@gmail.com>
2025-06-18 10:32:08 -0300

    Delete RECUPERAÇÃO JS2 directory

--- RECUPERAÇÃO JS2/src.js
+++
@@ -1,6 +0,0 @@
-const btnMostrar = document.getElementById("Mostrar");
-const mensagem = document.getElementById("mensagem");
-
-btnMostrar.addEventListener("click", function () {
-  mensagem.classList.remove("oculto");
-});
